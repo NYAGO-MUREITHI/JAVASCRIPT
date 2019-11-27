d2d888dc43ef414cae04ae6686165930a3badf41
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-11-27 08:10:27 +0300

    add js file

---
+++ html.js
@@ -0,0 +1,11 @@
+function add (number1,number2){
+return number1 + number2;
+}
+
+var number1= parseInt(prompt("Enter first number here"))
+alert(number1)
+
+var number2= parseInt(prompt("Enter second number here"))
+alert(number2)
+
+alert(add(number1,number2))
